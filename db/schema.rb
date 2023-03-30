90d18f9493472917ae2a7532fb018d45d02f6d00
Fuji-tori <out.fuji96aya@gmail.com>
2023-03-30 23:37:37 +0900

    space

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_01_26_162934) do
-
+  
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
     t.text "content"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_162934) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
   end
-
+  
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,5 +32,5 @@ ActiveRecord::Schema.define(version: 2020_01_26_162934) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
+  
 end
